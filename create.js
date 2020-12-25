f626755b343f88bd641580729032b1932d949b81
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2020-12-25 22:50:26 +0200

    changes added

---
+++ create.js
@@ -0,0 +1,10 @@
+import galleryItems from "./gallery-items.js";
+
+//console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector("js-gallery"),
+};
+
+refs.gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {}
